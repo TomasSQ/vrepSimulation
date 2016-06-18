ad81ec6fc2b92eb04cac84028f39de608a1e7bc0
Luciano P. Sabenca(luciano.sabenca@movile.com) <luciano.sabenca@movile.com>
2016-06-17 21:55:04 -0300

    Exemplo simples

---
+++ teste.py
@@ -0,0 +1,55 @@
+import vrep
+import time
+
+#para rodar, coloque na mesma pasta do vrep.
+#instrucoes em http://www.coppeliarobotics.com/helpFiles/en/remoteApiClientSide.htm
+print ('Program started')
+
+vrep.simxFinish(-1) # just in case, close all opened connections
+clientID = vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connecta com o VREP. Por padrao ele ja abre essa porta.
+if clientID == -1:
+    exit (10)
+
+print ('Connected to remote API server')
+
+
+def reset_simulation(clientID, lHipHandle, rHipHandle):
+    vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
+    vrep.simxSetJointTargetPosition(clientID,lHipHandle,0.2,vrep.simx_opmode_oneshot);
+    vrep.simxSetJointTargetPosition(clientID,rHipHandle,0.2,vrep.simx_opmode_oneshot);
+    time.sleep(1) # um pequeno sleep entre o stop e o start
+    vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
+
+
+# obtem os handlers. Um Handler eh um numero que identifica um componente, como, por exemplo, uma junta
+res, shL = vrep.simxGetObjectHandle(clientID, "LShoulderPitch3", vrep.simx_opmode_blocking)
+res, shR = vrep.simxGetObjectHandle(clientID, "RShoulderPitch3", vrep.simx_opmode_blocking)
+res, kneeR = vrep.simxGetObjectHandle(clientID, "RKneePitch3", vrep.simx_opmode_blocking)
+res, kneeL = vrep.simxGetObjectHandle(clientID, "LKneePitch3", vrep.simx_opmode_blocking)
+res, hipPitchL = vrep.simxGetObjectHandle(clientID, "LHipPitch3", vrep.simx_opmode_blocking)
+res, hipPitchR = vrep.simxGetObjectHandle(clientID, "RHipPitch3", vrep.simx_opmode_blocking)
+res, hipYawPitchL = vrep.simxGetObjectHandle(clientID, "LHipYawPitch3", vrep.simx_opmode_blocking)
+res, hipYawPitchR = vrep.simxGetObjectHandle(clientID, "RHipYawPitch3", vrep.simx_opmode_blocking)
+res, anklePitchL = vrep.simxGetObjectHandle(clientID, "LAnklePitch3", vrep.simx_opmode_blocking)
+res, anklePitchR = vrep.simxGetObjectHandle(clientID, "RAnklePitch3", vrep.simx_opmode_blocking)
+
+
+
+# Envia comandos para as juntas
+vrep.simxSetJointTargetPosition(clientID, hipPitchR, -1.2,vrep.simx_opmode_oneshot)
+vrep.simxSetJointTargetPosition(clientID, kneeR, 1.2,vrep.simx_opmode_oneshot)
+vrep.simxSetJointTargetPosition(clientID, anklePitchR, 0.4,vrep.simx_opmode_oneshot)
+
+time.sleep(1)
+vrep.simxSetJointTargetPosition(clientID, hipYawPitchL, -0.3,vrep.simx_opmode_oneshot)
+
+vrep.simxSetJointTargetPosition(clientID, hipPitchR, -0.1,vrep.simx_opmode_oneshot)
+vrep.simxSetJointTargetPosition(clientID, hipYawPitchR, -0.3,vrep.simx_opmode_oneshot)
+vrep.simxSetJointTargetPosition(clientID, kneeR, -0.1,vrep.simx_opmode_oneshot)
+vrep.simxSetJointTargetPosition(clientID, anklePitchR, 0,vrep.simx_opmode_oneshot)
+
+
+#time.sleep(3)
+#vrep.simxSetJointTargetPosition(clientID, hipPitchL, -0.5,vrep.simx_opmode_oneshot)
+#vrep.simxSetJointTargetPosition(clientID, kneeL, -0.5,vrep.simx_opmode_oneshot)
+#vrep.simxSetJointTargetPosition(clientID, anklePitchL, 0.5,vrep.simx_opmode_oneshot)
