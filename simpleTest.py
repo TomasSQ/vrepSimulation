cd3a7aa19621e7ae82070ff3adcce45606c44afc
Jucelio Fonseca <jfonseca345@gmail.com>
2016-06-24 19:12:07 -0300

    Primeiro algoritmo em gentéticos

--- simpleTest.py
+++ simpleTest.py
@@ -40,7 +40,7 @@ import time
 
 print ('Program started')
 vrep.simxFinish(-1) # just in case, close all opened connections
-clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to V-REP
+clientID=vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to V-REP
 if clientID!=-1:
     print ('Connected to remote API server')
 
