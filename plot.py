d9b8cd96f6f4a65c624e5798d5a669942f2d95a6
TomasSQ <tomasqueiroga@gmail.com>
2016-06-27 23:12:35 -0300

    minimos quadrados

---
+++ plot.py
@@ -0,0 +1,149 @@
+from deap import base
+from deap import creator
+from deap import tools
+import random
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+N_COEF = 100
+NGEN = 10
+POPULATION_SIZE = 100
+CXPB = 0.9
+MUTPB = 0.1
+BEGIN = 0
+END = BEGIN + 6000
+
+INTERVAL = 0.001
+
+def truncated_Fourier(coeficients, time):
+    value = coeficients[0]
+
+    for i in xrange(N_COEF):
+        value += coeficients[2 * i + 2] * math.cos(coeficients[1] * time)
+        value += coeficients[2 * i + 3] * math.sin(coeficients[1] * time)
+
+    return value
+
+def createEval(function):
+    def evalFit(individual):
+        fit = 0
+        for t in xrange(END - BEGIN):
+            fit += abs(function[t] - truncated_Fourier(individual, t * INTERVAL) / 4)
+
+        if fit > 100 and individual[1] == 0:
+            fit *= 1000
+        return fit,
+    return evalFit
+
+def mutate(individual):
+    return [(random.random() * 10 - 10 / 2.0) for x in individual if random.random() < 0.1]
+
+def initGA(function):
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMin)
+    toolbox = base.Toolbox()
+    toolbox.register("attr_coef", random.uniform, -10, 10)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_coef, 2 * N_COEF + 2)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    toolbox.register("evaluate", createEval(function))
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", mutate)
+    toolbox.register("elite",tools.selBest, k = 2)
+    toolbox.register("select", tools.selTournament, tournsize = 3)
+
+    return toolbox
+
+def evolve(toolbox):
+    pop = toolbox.population(n = POPULATION_SIZE)
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+
+    for g in range(NGEN):
+        print("-- Generation %i --" % g)
+
+        elite = toolbox.elite(pop)
+        elite = list(map(toolbox.clone, elite))
+        offspring = toolbox.select(pop, POPULATION_SIZE-2)
+        offspring = list(map(toolbox.clone, offspring))
+
+        for child1, child2 in zip(offspring[::2],offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+
+
+        for mutant in offspring:
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+
+        offspring = elite + offspring
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        if len(offspring) > 0:
+            pop[:] = offspring
+
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in pop]
+
+        length = len(pop)
+        mean = sum(fits) / length
+
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+
+        if min(fits) < 2:
+            break
+
+    return tools.selBest(pop, k = 1)[0]
+
+def aproximate(function):
+    toolbox = initGA(function)
+    return evolve(toolbox)
+
+f = file('../training_data2.txt')
+
+functions = []
+
+for i in xrange(27):
+    functions += [[]]
+
+for line in f:
+    split_line = line.split(' ')
+    if float(split_line[0]) < BEGIN:
+        continue
+    if float(split_line[0]) > END:
+        break
+    to_float = [float(x) for x in split_line[1:]]
+    for i in xrange(len(to_float)):
+        functions[i] += [to_float[i]]
+
+for function in functions[2:]:
+    x = np.arange(0, len(function) * INTERVAL, INTERVAL)
+    plt.plot(x, function)
+
+    #coeficients = aproximate(function)
+    #print coeficients
+    #aproximation = []
+    #for t in xrange(len(function)):
+    #    aproximation += [truncated_Fourier(coeficients, t * INTERVAL)]
+
+    #plt.plot(x, np.array(aproximation) / 4)
+    p = np.poly1d(np.polyfit(x, function, 150))
+    aproximation = []
+    for t in xrange(len(function)):
+        aproximation += [p(t * INTERVAL)]
+    plt.plot(x, aproximation)
+
+    plt.show()
--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import vrep
 import time
 from manage_joints import *
+import numpy as np
 
 #para rodar, coloque na mesma pasta do vrep.
 #instrucoes em http://www.coppeliarobotics.com/helpFiles/en/remoteApiClientSide.htm
@@ -56,16 +57,16 @@ def simple():
     #vrep.simxSetJointTargetPosition(clientID, anklePitchL, 0.5,vrep.simx_opmode_oneshot)
 
 
-def startPosition():
-    Head_Yaw=[];Head_Pitch=[];
-    L_Hip_Yaw_Pitch=[];L_Hip_Roll=[];L_Hip_Pitch=[];L_Knee_Pitch=[];L_Ankle_Pitch=[];L_Ankle_Roll=[];
-    R_Hip_Yaw_Pitch=[];R_Hip_Roll=[];R_Hip_Pitch=[];R_Knee_Pitch=[];R_Ankle_Pitch=[];R_Ankle_Roll=[];
-    L_Shoulder_Pitch=[];L_Shoulder_Roll=[];L_Elbow_Yaw=[];L_Elbow_Roll=[];L_Wrist_Yaw=[]
-    R_Shoulder_Pitch=[];R_Shoulder_Roll=[];R_Elbow_Yaw=[];R_Elbow_Roll=[];R_Wrist_Yaw=[]
-    R_H=[];L_H=[];R_Hand=[];L_Hand=[];
-    Body = [Head_Yaw,Head_Pitch,L_Hip_Yaw_Pitch,L_Hip_Roll,L_Hip_Pitch,L_Knee_Pitch,L_Ankle_Pitch,L_Ankle_Roll,R_Hip_Yaw_Pitch,R_Hip_Roll,R_Hip_Pitch,R_Knee_Pitch,R_Ankle_Pitch,R_Ankle_Roll,L_Shoulder_Pitch,L_Shoulder_Roll,L_Elbow_Yaw,L_Elbow_Roll,L_Wrist_Yaw,R_Shoulder_Pitch,R_Shoulder_Roll,R_Elbow_Yaw,R_Elbow_Roll,R_Wrist_Yaw,L_H,L_Hand,R_H,R_Hand]
+Head_Yaw=[];Head_Pitch=[];
+L_Hip_Yaw_Pitch=[];L_Hip_Roll=[];L_Hip_Pitch=[];L_Knee_Pitch=[];L_Ankle_Pitch=[];L_Ankle_Roll=[];
+R_Hip_Yaw_Pitch=[];R_Hip_Roll=[];R_Hip_Pitch=[];R_Knee_Pitch=[];R_Ankle_Pitch=[];R_Ankle_Roll=[];
+L_Shoulder_Pitch=[];L_Shoulder_Roll=[];L_Elbow_Yaw=[];L_Elbow_Roll=[];L_Wrist_Yaw=[]
+R_Shoulder_Pitch=[];R_Shoulder_Roll=[];R_Elbow_Yaw=[];R_Elbow_Roll=[];R_Wrist_Yaw=[]
+R_H=[];L_H=[];R_Hand=[];L_Hand=[];
+Body = [Head_Yaw,Head_Pitch,L_Hip_Yaw_Pitch,L_Hip_Roll,L_Hip_Pitch,L_Knee_Pitch,L_Ankle_Pitch,L_Ankle_Roll,R_Hip_Yaw_Pitch,R_Hip_Roll,R_Hip_Pitch,R_Knee_Pitch,R_Ankle_Pitch,R_Ankle_Roll,L_Shoulder_Pitch,L_Shoulder_Roll,L_Elbow_Yaw,L_Elbow_Roll,L_Wrist_Yaw,R_Shoulder_Pitch,R_Shoulder_Roll,R_Elbow_Yaw,R_Elbow_Roll,R_Wrist_Yaw,L_H,L_Hand,R_H,R_Hand]
 
-    get_all_handles(3, clientID,Body)
+get_all_handles(3, clientID,Body)
+def startPosition():
 
     f = file('start_moviment_ga.txt')
 
@@ -77,4 +78,50 @@ def startPosition():
         JointControl(clientID, 0, Body, move)
         time.sleep(0.1)
 
+BEGIN = 4210
+END = BEGIN + 3000
+
+INTERVAL = 0.001
+
+
+def walk():
+    f = file('training_data2.txt')
+
+    functions = []
+    aproximations = []
+
+    for i in xrange(26):
+        functions += [[]]
+        aproximations += [[]]
+
+    for line in f:
+        split_line = line.split(' ')
+        if float(split_line[0]) < BEGIN:
+            continue
+        if float(split_line[0]) > END:
+            break
+        to_float = [float(x) for x in split_line[1:]]
+        for i in xrange(len(to_float)):
+            functions[i] += [to_float[i]]
+
+    i = 0
+    for f in xrange(len(functions)):
+        print i
+        i += 1
+        x = np.arange(0, len(functions[f]) * INTERVAL, INTERVAL)
+        p = np.poly1d(np.polyfit(x, functions[f], 150))
+        for t in xrange(len(functions[f])):
+            aproximations[f] += [p(t * INTERVAL)]
+
+    while True:
+        for t in xrange(len(aproximations[0])):
+            move = []
+            for aproximation in aproximations:
+                move += [aproximation[t]]
+            print t
+            JointControl(clientID, 0, Body, move)
+            time.sleep(0.007)
+
+reset_simulation(clientID)
 startPosition()
+walk()
